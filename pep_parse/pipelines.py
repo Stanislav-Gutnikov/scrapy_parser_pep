331d5548d89dfda20652c3c5b1c32ee60b2108cb
Стас Гутников <stasgutnikov0853@gmail.com>
2023-05-18 18:51:50 +0300

    2

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import csv
 from pathlib import Path
 
 
@@ -13,20 +14,23 @@ class PepParsePipeline:
         pass
 
     def process_item(self, item, spider):
-        if item['status'] not in self.statuses.keys():
-            self.statuses[item['status']] = 1
-        else:
-            self.statuses[item['status']] += 1
+        self.statuses[item['status']] = self.statuses.get(
+            item['status'], 0
+        ) + 1
         res_dir = BASE_DIR / 'results'
         res_dir.mkdir(exist_ok=True)
         time = dt.datetime.now()
         time = time.strftime('%Y-%m-%d_%H-%M')
         filename = res_dir / f'status_summary_{time}.csv'
         with open(filename, mode='w', encoding='utf-8') as f:
-            f.write('Статус,Количество\n')
-            for key, value in self.statuses.items():
-                f.write(f'{key},{value}\n')
-            f.write(f'Total,{sum(self.statuses.values())}\n')
+            writer = csv.writer(f)
+            writer.writerows(
+                (
+                    ('Статус', 'Количество'),
+                    *self.statuses.items(),
+                    ('Total', sum(self.statuses.values()))
+                )
+            )
         return item
 
     def close_spider(self, spider):
